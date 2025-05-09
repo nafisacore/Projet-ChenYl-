ee9f5ded07ccf6f953bee98e3d4bcdbbaac80293
nafisacore <nafisa.muhammad@hotmail.com>
2025-05-09 19:06:59 +0200

    Update ajt_animal.c

--- ajt_animal.c
+++ ajt_animal.c
@@ -2,14 +2,14 @@
 
 
 //Procédure ajouter un animal
-void ajouter_animal() {
-    if (nb_animaux >= MAX_ANIMAUX) {
+void ajouter_animal(Animal** animaux, int* nb_animaux) {
+    if (*nb_animaux >= MAX_ANIMAUX) {
         printf("Le nombre maximal d'animaux est atteint.\n");
         return;
     }
 
     Animal nouvel_animal;
-    nouvel_animal.id = nb_animaux + 1;  
+    nouvel_animal.id = *nb_animaux + 1;  
 
     // Saisie des infos par l'utilisateur
 
@@ -42,18 +42,18 @@ void ajouter_animal() {
     scanf(" %[^\n]", nouvel_animal.commentaire);  // Prend toute la ligne avec espaces
 
     // On agrandit le tableau pour ajouter l’animal
-    animaux = realloc(animaux, (nb_animaux + 1) * sizeof(Animal));
-    if (animaux == NULL) {
+    *animaux = realloc(*animaux, (*nb_animaux + 1) * sizeof(Animal));
+    if (*animaux == NULL) {
         printf("Erreur d'allocation mémoire.\n");
         exit(6);
     }
 
     // On met l’animal à la fin du tableau
-    animaux[nb_animaux] = nouvel_animal;
-    nb_animaux++;
+    (*animaux)[*nb_animaux] = nouvel_animal;
+    (*nb_animaux)++;
 
-    sauvegarder_animaux();        
-    sauvegarder_nombre_animaux();   
+    sauvegarder_animaux(*animaux, *nb_animaux);        
+    sauvegarder_nombre_animaux(*nb_animaux);   
 
     printf("L'animal a été ajouté avec succès !\n");
 
