46f81677f0ddea23d5b824dee876b5235c5cf0d2
nafisacore <nafisa.muhammad@hotmail.com>
2025-05-09 19:07:43 +0200

    Update supp_animal.c

--- supp_animal.c
+++ supp_animal.c
@@ -1,6 +1,6 @@
 #include "animal.h"
 
-void supp_animal() {
+void supp_animal(Animal** animaux, int* nb_animaux) {
     int id_suppression;
     printf("Entrez l'ID de l'animal adopté : ");
     scanf("%d", &id_suppression);
@@ -13,20 +13,26 @@ void supp_animal() {
     }
 
     int trouve = 0;
-    for (int i = 0; i < nb_animaux; i++) {
-        if (animaux[i].id == id_suppression) {
+    for (int i = 0; i < *nb_animaux; i++) {
+        if ((*animaux)[i].id == id_suppression) {
             // Libère la mémoire du commentaire de l’animal à supprimer
-            free(animaux[i].commentaire);
+            free((*animaux)[i].commentaire);
 
             // Décale les suivants vers la gauche
-            for (int j = i; j < nb_animaux - 1; j++) {
-                animaux[j] = animaux[j + 1];
+            for (int j = i; j < *nb_animaux - 1; j++) {
+                (*animaux)[j] = (*animaux)[j + 1];
             }
 
-            nb_animaux--;
+            (*nb_animaux)--;
 
-            sauvegarder_animaux();
-            sauvegarder_nombre_animaux();
+            *animaux = realloc(*animaux, (*nb_animaux) * sizeof(Animal));
+            if (*animaux == NULL && *nb_animaux > 0) {
+                printf("Erreur de réallocation mémoire.\n");
+                exit(30);
+            }
+
+            sauvegarder_animaux(*animaux, *nb_animaux);
+            sauvegarder_nombre_animaux(*nb_animaux);
 
             printf("Animal supprimé.\n");
             trouve = 1;
