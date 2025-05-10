771cca1d5d685a8c7c381f3a0790a8aad48400fb
nafisacore <nafisa.muhammad@hotmail.com>
2025-05-10 21:12:27 +0200

    Update ajt_animal.c

--- ajt_animal.c
+++ ajt_animal.c
@@ -1,33 +1,32 @@
 #include "animal.h"
 
-
-//Procédure ajouter un animal
-void ajouter_animal(Animal* animaux, int* nb_animaux) {
+// Procédure ajouter un animal
+void ajouter_animal(Animal** animaux, int* nb_animaux) {
     if (*nb_animaux >= MAX_ANIMAUX) {
         printf("Le nombre maximal d'animaux est atteint.\n");
         return;
     }
 
     Animal nouvel_animal;
-    nouvel_animal.id = *nb_animaux + 1;  
+    nouvel_animal.id = *nb_animaux + 1;
 
     // Saisie des infos par l'utilisateur
 
-    //nom
+    // Nom
     printf("Entrez le nom de l'animal: ");
     scanf("%s", nouvel_animal.nom);
     
-    //espece
-    printf("Entrez l'espece (chien, chat, hamster, autruche): ");
+    // Espèce
+    printf("Entrez l'espèce (chien, chat, hamster, autruche): ");
     char race[20];
     scanf("%s", race);
     nouvel_animal.espece = chaine_en_espece(race);
 
-    //année de naissance
+    // Année de naissance
     printf("Entrez l'année de naissance de l'animal: ");
     scanf("%d", &nouvel_animal.annee_naissance);
 
-    //poids
+    // Poids
     printf("Entrez le poids de l'animal en kg: ");
     scanf("%f", &nouvel_animal.poids);
 
@@ -42,19 +41,19 @@ void ajouter_animal(Animal* animaux, int* nb_animaux) {
     scanf(" %[^\n]", nouvel_animal.commentaire);  // Prend toute la ligne avec espaces
 
     // On agrandit le tableau pour ajouter l’animal
-    animaux = realloc(animaux, (*nb_animaux + 1) * sizeof(Animal));
-    if (animaux == NULL) {
+    *animaux = realloc(*animaux, (*nb_animaux + 1) * sizeof(Animal));
+    if (*animaux == NULL) {
         printf("Erreur d'allocation mémoire.\n");
         exit(6);
     }
 
     // On met l’animal à la fin du tableau
-    animaux[*nb_animaux] = nouvel_animal;
+    (*animaux)[*nb_animaux] = nouvel_animal;
     (*nb_animaux)++;
 
-    sauvegarder_animaux(animaux, *nb_animaux);        
+    sauvegarder_animaux(*animaux, *nb_animaux);        
     sauvegarder_nombre_animaux(*nb_animaux);   
 
     printf("L'animal a été ajouté avec succès !\n");
+}
 
-} 
