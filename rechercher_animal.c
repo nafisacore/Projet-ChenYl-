90c5e772aa9155a7b5a553d25daa9366c0fbb3c6
nafisacore <nafisa.muhammad@hotmail.com>
2025-05-11 00:35:40 +0200

    Update rechercher_animal.c

--- rechercher_animal.c
+++ rechercher_animal.c
@@ -1,18 +1,20 @@
 #include "animal.h"
 
-
 void rechercher_animal(Animal* animaux, int nb_animaux) {
     int choixx, veriff;
-    
-    do{
-        printf("Souhaitez-vous rechercher l'animal par :\n1. Son nom\n2. Son ID\n3. Son type d'âge\n4. Quitter\nChoisissez : ");
+
+    do {
+        printf("\nSouhaitez-vous rechercher l'animal par :\n1. Son nom\n2. Son ID\n3. Son type d'âge\n4. Quitter\nChoisissez : ");
+        
         veriff = scanf("%d", &choixx);
 
+        // Vérification de l'entrée
         if (veriff != 1) {
-            printf("Entree invalide, reessayer : \n");
-            vide_buffer();
+            printf("\nEntrée invalide, veuillez réessayer.\n");
+            vide_buffer();  // Vide le buffer d'entrée pour éviter des erreurs futures
             continue;
         }
+
 
         switch (choixx) {
             case 1:
@@ -25,11 +27,10 @@ void rechercher_animal(Animal* animaux, int nb_animaux) {
                 rechercher_par_age(animaux, nb_animaux);
                 break;
             case 4:
-                printf("AU revoir !");
+                printf("Au revoir !\n");
                 break;
-            default :
-                printf("Choix invalide, choisissez un chiffre entre 1 et 4:");    
-        }    
-    }while(choixx != 1 && choixx != 2 && choixx != 3 && choixx != 4);
-               
+            default:
+                break;
+        }
+    } while (choixx != 1 && choixx != 2 && choixx != 3 && choixx != 4); 
 }
